06ca9bebc05af7f2c2cf39c96f025cad3d28a4ea
emon3455 <emon.mhk69@gmail.com>
2023-02-20 01:48:24 +0600

    finding minimum maximum from an array

---
+++ js/max-Min.js
@@ -0,0 +1,10 @@
+
+const number = [10,20,30,40,50,60,70];
+
+// finding max:
+const maximum = Math.max(...number);
+console.log("Maximum number:",maximum);
+
+// finding min:
+const minimum = Math.min(...number);
+console.log("Minimum number:",minimum);
