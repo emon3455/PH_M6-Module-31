5012dd6199423327a62c5f1c775a5886b664ed27
emon3455 <emon.mhk69@gmail.com>
2023-02-20 00:01:43 +0600

    dom manipulation recap done

--- js/dom.js
+++ js/dom.js
@@ -9,6 +9,24 @@ document.getElementById("bg-added").addEventListener("click" , function(){
 });
 
 document.getElementById("center-3rd").addEventListener("click", function(){
+
     const friend = document.getElementById("3rd-friend");
     friend.style.textAlign="center";
+
+});
+
+document.getElementById("add-friend").addEventListener("click", function(){
+
+    const friendContainer = document.getElementById("friend");
+
+    const friend = document.createElement("div");
+    friend.classList.add("friend");
+    friend.innerHTML=`
+    <div class="friend">
+        <h3 class="friend-name">New Fried</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, soluta!</p>
+    </div>
+    `;
+    friendContainer.appendChild(friend);
+
 });
