c3481e2ae7e44ef5aa85d36f0733ef7815fcf07e
emon3455 <emon.mhk69@gmail.com>
2023-02-20 01:36:46 +0600

    more about arrow function

--- js/arrow.js
+++ js/arrow.js
@@ -4,9 +4,41 @@ function add(first,second){
 }
 
 // anonimas function :(j function ar nam nai)
-const add = function(first,second){
+const add1 = function(first,second){
     return first + second;
 }
 
 // arrow function:
 const sum = (first, second) => first+second;
+
+
+// no parameter arrow function:
+const printing = ()=> "Hellow world";
+const result1 = printing();
+console.log(result1);
+
+// single parameter arrow function:
+
+// Way-1
+const double = (num) => num*2;
+const result2 = double(2);
+console.log(result2);
+
+// way-2:
+const square = num => num*num;
+const result3 = square(5);
+console.log(result3);
+
+
+// multi line arro function
+const calculate = (num1,num2)=>{
+
+    let sum = num1 + num2;
+    let square = sum * sum;
+    let devide = square/2;
+
+    return devide;
+}
+
+const result4 = calculate(2,4);
+console.log(result4);
