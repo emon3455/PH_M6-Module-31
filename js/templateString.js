bc5e14b1bad0fdc31a5042c7f10391d9873353ac
emon3455 <emon.mhk69@gmail.com>
2023-02-20 00:47:28 +0600

    template string done

---
+++ js/templateString.js
@@ -0,0 +1,12 @@
+// by using back tik we can write multi line string easyly:
+const introduction = `Hello !... I am Emon,
+Welcome to my website `;
+
+console.log(introduction);
+
+// dynamic string:
+const name = "Emon";
+const age = 21;
+const birthdate = "05-03-2002";
+const aboutME = ` Hello i am ${name}. I am ${age} years old. My birthdate is ${birthdate} `;
+console.log(aboutME);
