e2531dba7323aeb397d39c06d36a64fbba0cc11c
emon3455 <emon.mhk69@gmail.com>
2023-02-20 02:43:55 +0600

    practice problem-1 done

---
+++ js/1problem.js
@@ -0,0 +1,12 @@
+const multiply = (x,y,z)=> x*y*z;
+console.log(multiply(1,2,3));
+
+const message = 
+`I am a web developer.
+I love to code.
+I love to eat biryani.
+`;
+console.log(message);
+
+const add = (x,y=0)=> x+y;
+console.log(add(2));
