341263df6a322a48ec1a6a97fb79a1424126d5a7
emon3455 <emon.mhk69@gmail.com>
2023-02-20 01:06:17 +0600

    basic arrow function intrduction done

---
+++ js/arrow.js
@@ -0,0 +1,12 @@
+// normall function
+function add(first,second){
+    return first+second;
+}
+
+// anonimas function :(j function ar nam nai)
+const add = function(first,second){
+    return first + second;
+}
+
+// arrow function:
+const sum = (first, second) => first+second;
